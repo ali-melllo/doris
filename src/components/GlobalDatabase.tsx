86bd3d753a4d8ed59c3dabca98f1ae353368d34f
ali-melllo <alimelllo32@gmail.com>
2025-05-27 12:59:26 +0330

    link fix

--- src/components/GlobalDatabase.tsx
+++ src/components/GlobalDatabase.tsx
@@ -1,6 +1,7 @@
 "use client"
 import createGlobe from "cobe"
 import { Baby, Briefcase, BriefcaseMedical, HomeIcon, Landmark, Map, Sparkles } from "lucide-react"
+import Link from "next/link"
 import { FunctionComponent, useEffect, useRef } from "react"
 
 export const GlobalDatabase: FunctionComponent = () => {
@@ -94,10 +95,10 @@ export const GlobalDatabase: FunctionComponent = () => {
                   <Briefcase className="stroke-primary" />
                   Jobs
                 </span>
-                <span className="size-24 font-semibold text-sm flex flex-col gap-2 items-center justify-center rounded-2xl bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
+                <Link href={'/chat'} className="size-24 font-semibold text-sm flex flex-col gap-2 items-center justify-center rounded-2xl bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
                   <Sparkles className="stroke-primary" />
                   AI Chat
-                </span>
+                </Link>
               </div>
               <div className="flex flex-row items-center justify-center gap-3">
                 <span className="size-24 font-semibold text-sm flex flex-col gap-2 items-center justify-center rounded-2xl bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]">
